182b1f4dd74ae08b96dd5d4f7eee9f0173135222
Benjamin Wright <y0rke@proton.me>
2023-03-18 18:05:05 +0800

    Open and close window

--- main.cpp
+++ main.cpp
@@ -1,5 +1,27 @@
-#include <iostream>
+#include <cstdio>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 int main(int, char**) {
-    std::cout << "Hello, world!\n";
+    printf("Hello World!\n");
+
+    if(!glfwInit()){
+        printf("GLFW Init failed!");
+    }
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+    if(!window){
+        printf("GLFW window creation failed!");
+    }
+
+    while(!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
+    return 0;
 }
